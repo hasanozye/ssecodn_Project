ce4561740af665cee9dbc2f11e806f80704007b0
Hasan Ozyer <hasanlareli@gmail.com>
2023-04-16 12:51:59 +0300

    OLAA SINYORRRR

---
+++ src/MainApp.java
@@ -0,0 +1,18 @@
+/**
+ * This is starting class
+ * @link
+ */
+
+public class MainApp {
+
+    // This starts a method.
+    public static void main(String[] args) {
+        System.out.println("Bye Bitch!");
+        System.out.println("Bye Bitch!");
+        System.out.println("Hi Worldzz");
+
+        //TODO: Bu kısımda toplama işlemi yapılacaktır.
+
+
+    }
+}
